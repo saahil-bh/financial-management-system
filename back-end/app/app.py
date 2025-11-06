77a2d2016a7a5183eb87b47336fa11bef6dbdf8d
Ponlapat Subsaenudom <psubsaenudom@gmail.com>
2025-11-06 20:42:16 +0700

    Add files via upload

--- back-end/app/app.py
+++ back-end/app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Annotated
 from fastapi.security import OAuth2PasswordRequestForm
@@ -11,6 +12,20 @@ from decimal import Decimal
 from datetime import timedelta
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 
 db_model.Base.metadata.create_all(bind=engine)
